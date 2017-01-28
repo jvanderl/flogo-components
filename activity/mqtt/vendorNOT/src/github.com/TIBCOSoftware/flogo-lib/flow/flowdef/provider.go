2f6425f235ce552ac65c949c241f262f96e3fca6
jvanderl <jvanderl@tibco.com>
2017-01-28 15:08:35 +0100

    Added MQTT Publisher

--- activity/mqtt/vendorNOT/src/github.com/TIBCOSoftware/flogo-lib/flow/flowdef/provider.go
+++
@@ -1,9 +0,0 @@
-package flowdef
-
-// Provider is the interface that describes an object
-// that can provide flow definitions from a URI
-type Provider interface {
-
-	// GetFlow retrieves the flow definition for the specified URI
-	GetFlow(flowURI string) *Definition
-}
