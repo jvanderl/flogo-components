2dde17596954b89168e39049ba0b5f6a69c40408
jvanderl <jvanderl@tibco.com>
2017-05-01 20:35:29 +0200

    new structure

--- activity/combine/activity_test.go
+++ activity/combine/activity_test.go
@@ -12,12 +12,12 @@ var activityMetadata *activity.Metadata
 
 func getActivityMetadata() *activity.Metadata {
 	if activityMetadata == nil {
-			jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
-			if err != nil{
-				panic("No Json Metadata found for activity.json path")
-			}
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
 		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
- 	}
+	}
 	return activityMetadata
 }
 
